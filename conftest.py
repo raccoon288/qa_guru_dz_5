cf75f908c658c971f8642da5114d120077a3f9ed
Valeria Oshurkova <v.osh.frrr@gmail.com>
2024-10-06 14:08:23 +0300

    New -- Добавлена фикстура

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_management():
+    browser.config.base_url = "https://demoqa.com"
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+    yield
+    browser.quit()
